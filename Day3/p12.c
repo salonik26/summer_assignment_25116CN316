1890f7a8702bd012325dda9451f2ee5cd2937c4b
salonik26 <salonirai.dec@gmail.com>
2026-06-03 23:44:36 +0530

    Day 3 Assignment

---
+++ Day3/p12.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    int lcm = (a * b) / gcd(a, b);
+
+    printf("LCM = %d\n", lcm);
+
+    return 0;
+}
---
+++ Day4/p14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int fibonacci(int n)(
+        if(n<=0){
+            printf("Does not exist");
+        }
+        int f1=0,f2=1,f3,
+    )
+
+}
