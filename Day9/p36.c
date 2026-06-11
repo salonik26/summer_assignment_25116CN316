074a4518153579b34a5f8e82e8f7e7010810f4cd
salonik26 <salonirai.dec@gmail.com>
2026-06-11 12:40:45 +0530

    Day9 Assignment

---
+++ Day9/p36.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int size,i,j;
+    printf("Enter the size of square:");
+    scanf("%d",&size);
+
+    for(i=1;i<=size;i++){
+        for(j=1;j<=size;j++){
+            if(i==1||i==size||j==1||j==size){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
