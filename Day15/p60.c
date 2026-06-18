38b806ca6084bec64a982d27dc6caeb0ce2a2ded
salonik26 <salonirai.dec@gmail.com>
2026-06-18 12:10:12 +0530

    Day15 Assignment

---
+++ Day15/p60.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int a[100],n,i,j,temp;
+
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+
+    printf("Enter the element:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-i-1;j++){
+            if(a[j]==0 && a[j+1]!=0){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+
+        }
+    }
+    printf("Array after moving zeroes:\n");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
